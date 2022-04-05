f21f8ba22be240e8db4101c025487483ca09645b
Serhii Rybak <rybaksw@gmail.com>
2022-04-05 15:13:16 +0300

    add simpleligthbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,33 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+galleryRef.insertAdjacentHTML('afterbegin', createGalleryMarkup(galleryItems));
+galleryRef.addEventListener('click', onClickGalleryImage);
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ description, original, preview }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        `;
+    })
+    .join('');
+}
+
+function onClickGalleryImage(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+}
+
+const imgGallery = new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
