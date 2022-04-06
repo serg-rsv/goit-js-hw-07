b039d8f5b7beac54a5e6cb970fa3de2bbcbfd7a2
Serhii Rybak <rybaksw@gmail.com>
2022-04-06 15:44:21 +0300

    Update 02-lightbox.js
    *remove needless code
    *correct markup li

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,26 +6,19 @@ console.log(galleryItems);
 const galleryRef = document.querySelector('.gallery');
 
 galleryRef.insertAdjacentHTML('afterbegin', createGalleryMarkup(galleryItems));
-galleryRef.addEventListener('click', onClickGalleryImage);
 
 function createGalleryMarkup(items) {
   return items
     .map(({ description, original, preview }) => {
       return `
+      <li>
         <a class="gallery__item" href="${original}">
           <img class="gallery__image" src="${preview}" alt="${description}" />
         </a>
+      </li>
         `;
     })
     .join('');
-}
-
-function onClickGalleryImage(e) {
-  e.preventDefault();
-
-  if (e.target.nodeName !== 'IMG') {
-    return;
-  }
 }
 
 const imgGallery = new SimpleLightbox('.gallery__item', {
